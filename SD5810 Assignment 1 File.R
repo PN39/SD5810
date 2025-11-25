628b3020da8e0b69017ce3921b3af0a1ac60684d
PN39 <pn39@st-andrews.ac.uk>
2025-11-25 22:13:36 +0800

    Added a plot

--- SD5810 Assignment 1 File.R
+++ SD5810 Assignment 1 File.R
@@ -2,3 +2,14 @@ install.packages("ggplot2")
 install.packages("dplyr")
 library(ggplot2)
 library(dplyr)
+
+df <- data.frame(
+  x = 1:10,
+  y = (1:10)^2
+)
+
+df_filtered <- dplyr::filter(df, x > 5)
+
+ggplot(df_filtered, aes(x = x, y = y)) +
+  geom_point() +
+  ggtitle("Example plot")
